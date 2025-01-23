1df5792bfd913458d6e970463b53c2863f373d63
Scroller <roman.pod2016@yandex.ru>
2025-01-23 22:20:31 +0300

    Добавил картинок

---
+++ app/src/main/java/com/example/lister/ui/theme/Kitten1.kt
@@ -0,0 +1,3 @@
+package com.example.lister.ui.theme
+
+data class Kitten1(val imageId: Int, val comment: String)
---
+++ app/src/main/java/com/example/lister/ui/theme/Kitten_adapter.kt
@@ -0,0 +1,11 @@
+package com.example.lister.ui.theme
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class Kitten_adapter: RecyclerView.Adapter<Kitten_adapter.Kittenshow> {
+
+    class Kittenshow(item: View): RecyclerView.ViewHolder(item) {
+
+    }
+}

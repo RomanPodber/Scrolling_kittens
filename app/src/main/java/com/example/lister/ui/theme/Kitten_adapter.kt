3933dfb8fba1863617eaf38204a7a43379b920b5
Scroller <roman.pod2016@yandex.ru>
2025-01-25 16:14:06 +0300

    По гайдам из интернетов леплю адаптер, но честно говоря мало что понятно

--- app/src/main/java/com/example/lister/ui/theme/Kitten_adapter.kt
+++ app/src/main/java/com/example/lister/ui/theme/Kitten_adapter.kt
@@ -1,13 +1,31 @@
 package com.example.lister.ui.theme
 
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.lister.databinding.KittenitemBinding
 
-class Kitten_adapter: RecyclerView.Adapter<Kitten_adapter.Kittenshow> {
+class Kitten_adapter: RecyclerView.Adapter<Kitten_adapter.Kittenshow>(){
 
     class Kittenshow(item: View): RecyclerView.ViewHolder(item) {
-        fun bind(kitten: Kitten1){
-
+        val binding = KittenitemBinding.bind(item)
+        fun bind(kitten: Kitten1)= with(binding){
+            imageView.setImageResource(kitten.imageId)
+            textview.text = kitten.comment
         }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Kittenshow {
+        val view = LayoutInflater.from(parent.context)
+        return TODO("Provide the return value")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: Kittenshow, position: Int) {
+        TODO("Not yet implemented")
     }
 }
